b15dc66e468d5951ee99091a65c912456119e7ef
mikemiksch <mikemiksch@gmail.com>
2016-11-07 11:17:16 -0800

    Changing names in keeping with updated file names

--- constructor.js
+++ constructor.js
@@ -18,17 +18,18 @@ function Dog(name, breed, size, age, sex, fixed, neighborhood, prefAge, prefSize
   this.prefSize = prefSize;
   this.prefSex = prefSex;
   this.prefFixed = prefFixed;
-  this.fileName = fileName;
+  this.filePath = 'Assets/images/profile-photos/' + fileName;
   this.liked = false;
   dogs.push(this);
 }
 
-new Dog('Thor', 'German Shepherd', 'Large', 4, 'Male', true, 'Capitol Hill', 'About the same', 'Large', 'Female', 'No Preference', 'Thor.jpg');
-new Dog('Jean Luc', 'French Bulldog', 'Small', 8, 'Male', true, 'Downtown Seattle', 'Older', 'Small', 'No Preference', 'No Preference', 'JeanLuc.jpg');
-new Dog('Smooshy', 'Pug', 'Small', 1, 'Female', false, 'West Seattle', 'Younger', 'No Preference', 'No Preference', 'Fixed', 'Smooshy.jpg');
-new Dog('YooDub', 'Husky', 'Medium', 2, 'Female', true, 'University District', 'No Preference', 'No Preference', 'No Preference', 'No Preference', 'YooDub.jpg');
-new Dog('Yukon', 'Golden Retriever', 'Large', 9, 'Male', true, 'Ballard', 'About the same', 'Large', 'Male', 'Fixed', 'Yukon.jpg');
-new Dog('Screw Ball', 'Mixed Breed', 3, 'Small', 'Female', false, 'Queen Anne', 'Older', 'Small', 'Female', 'Fixed', 'ScrewBall.jpg');
-new Dog('Atlas, Titan of the Sky', 'Corgi', 5, 'Medium', 'Male', false, 'Belltown', 'Younger', 'No Preference', 'No Preference', 'Intact', 'Atlas.jpg');
-new Dog('Armando Christian Pérez', 'Pit Bull', 6, 'Large', 'Male', true, 'Freemont', 'No Preference', 'Large', 'Male', 'No Preference', 'Armando.jpg');
-new Dog('Cat', 'Yellow Lab', 7, 'Large', 'Female', false, 'Green Lake', 'Younger', 'Medium', 'Female', 'No Preference', 'Cat.jpg');
+new Dog('Allison', 'German Shepherd', 'Large', 4, 'Female', true, 'Capitol Hill', 'About the same', 'Large', 'Female', 'No Preference', 'allison.jpg');
+new Dog('Cassius', 'French Bulldog', 'Small', 8, 'Male', true, 'Downtown Seattle', 'Older', 'Small', 'No Preference', 'No Preference', 'cassius.jpg');
+new Dog('Buggy', 'Pug', 'Small', 1, 'Female', false, 'West Seattle', 'Younger', 'No Preference', 'No Preference', 'Fixed', 'buggy.jpg');
+new Dog('Zeke', 'Husky', 'Medium', 2, 'Male', true, 'University District', 'No Preference', 'No Preference', 'No Preference', 'No Preference', 'zeke.jpg');
+new Dog('Cheri', 'Golden Retriever', 'Large', 9, 'Feale', true, 'Ballard', 'About the same', 'Large', 'Male', 'Fixed', 'cheri.jpg');
+new Dog('Lili', 'Mixed Breed', 3, 'Small', 'Female', false, 'Queen Anne', 'Older', 'Small', 'Female', 'Fixed', 'lili.jpg');
+new Dog('Stan', 'Corgi', 5, 'Medium', 'Male', false, 'Belltown', 'Younger', 'No Preference', 'No Preference', 'Intact', 'stan.jpg');
+new Dog('Clara', 'Pit Bull', 6, 'Large', 'Female', true, 'Freemont', 'No Preference', 'Large', 'Male', 'No Preference', 'clara.jpg');
+new Dog('Murphy', 'Yellow Lab', 7, 'Large', 'Male', false, 'Green Lake', 'Younger', 'Medium', 'Female', 'No Preference', 'murphy.jpg');
+new Dog('Chance', 'Yellow Lab', 4, 'Large', 'Male', true, 'Beacon Hill', 'About the same', 'No Preference', 'No Preference', 'No Preference', 'chance.jpg');
