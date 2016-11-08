f926db26464b5be8e5a2b5455f3a55251d29661a
mikemiksch <mikemiksch@gmail.com>
2016-11-08 14:02:14 -0800

    Doing some work on getting results to display and link to profile page

--- js/display-results.js
+++ js/display-results.js
@@ -2,15 +2,25 @@
 
 // Taking match results from previous page
 var rankedDogs = JSON.parse(localStorage['dogs']);
+var topDogs = 3;
 
 // General function to make new HTML elements
-function makeNewElement(tag, innerText, parent, childId, cssClass) {
+function makeNewContainingElement(tag, innerText, parent, childId, cssClass) {
   var parent = document.getElementById(parent);
   var newEl = document.createElement(tag);
   newEl.innerText = innerText;
   newEl.setAttribute('id', childId);
   newEl.setAttribute('class', cssClass);
   parent.appendChild(newEl);
+}
+
+function makeNewAnchor(href, parent, childId, cssClass) {
+  var parent = document.getElementById(parent);
+  var newAnchor = document.createElement('a');
+  newAnchor.setAttribute('href', href);
+  newAnchor.setAttribute('id', childId);
+  newAnchor.setAttribute('class', cssClass);
+  parent.appendChild(newAnchor);
 }
 
 function makeNewImage(src, parent) {
@@ -22,14 +32,15 @@ function makeNewImage(src, parent) {
 
 // Renders the HTML of the profiles from the objects in the array
 function makeProfiles() {
-  for (var i = 0; i < 3; i++) {
-    makeNewElement('div', '', 'results', rankedDogs[i].name, 'profile-box');
-    makeNewElement('div', '', rankedDogs[i].name, rankedDogs[i].name + '-pic', 'profile-pic');
-    makeNewImage(rankedDogs[i].filePath, rankedDogs[i].name + '-pic');
-    makeNewElement('div', '', rankedDogs[i].name, rankedDogs[i].name + '-name', 'profile-name');
-    makeNewElement('p', rankedDogs[i].name, rankedDogs[i].name + '-name', '', '');
-    makeNewElement('div', '', rankedDogs[i].name, rankedDogs[i].name + '-percent', 'match-percent');
-    makeNewElement('p', rankedDogs[i].matchPercentage + '%', rankedDogs[i].name + '-percent', rankedDogs[i].name + '-percent-text', '');
+  for (var i = 0; i < topDogs; i++) {
+    makeNewContainingElement('div', '', 'results', rankedDogs[i].name, 'profile-box');
+    makeNewContainingElement('div', '', rankedDogs[i].name, rankedDogs[i].name + '-pic', 'profile-pic');
+    makeNewAnchor('profile.html', rankedDogs[i].name + '-pic', i, 'profile-link');
+    makeNewImage(rankedDogs[i].filePath, i, 'clickable');
+    makeNewContainingElement('div', '', rankedDogs[i].name, rankedDogs[i].name + '-name', 'profile-name');
+    makeNewContainingElement('p', rankedDogs[i].name, rankedDogs[i].name + '-name', '', '');
+    makeNewContainingElement('div', '', rankedDogs[i].name, rankedDogs[i].name + '-percent', 'match-percent');
+    makeNewContainingElement('p', rankedDogs[i].matchPercentage + '%', rankedDogs[i].name + '-percent', rankedDogs[i].name + '-percent-text', '');
   }
 }
 
@@ -47,6 +58,19 @@ function percentageColor() {
       percentText.setAttribute('class', 'poor-match');
     }
   }
+}
+
+// Event listener for capturing which picture gets clicked on and passed to the profile page.
+
+function getClickedArrayIndex(event) {
+  var profileClick = event.target.id;
+  localStorage.setItem('profileClick', profileClick);
+}
+
+var anchorTags = document.getElementsByClassName('profile-link');
+for (var i = 0; i < anchorTags.length; i++) {
+  console.log('hi there');
+  anchorTags[i].addEventListener('click', getClickedArrayIndex);
 }
 
 // The function that does everying at once.
