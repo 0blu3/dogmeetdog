eed24e3df6df001043dc3ea16ceaea37fef73d9d
Christina Thomas <Penssake@gmail.com>
2016-11-08 13:40:49 -0800

    seperate js file work so far

---
+++ js/form.js
@@ -0,0 +1,42 @@
+'use strict!';
+
+function submit(event) {
+  event.preventDefault();
+  var inputName = event.target.name.value;
+  localStorage.setItem('name', name.value);
+  console.log(inputName);
+  var inputNumber = event.target.contact_number.value;
+  localStorage.setItem('contact_number', contact_number.value);
+  var inputEmail = event.target.inputEmail.value;
+  localStorage.setItem('email', email.value);
+  var inputPupName = event.target.inputPupName.value;
+  localStorage.setItem('pups_name', pups_name.value);
+  var inputBreed = event.target.breed.value;
+  localStorage.setItem('breed', breed.value);
+  var inputGender = event.target.gender.value;
+  localStorage.setItem('gender', gender.value);
+  var inputAge = event.target.age.value;
+  localStorage.setItem('age', age.value);
+  console.log(age);
+  var inputSwimming = event.target.swimming.value;
+  console.log(inputSwimming);
+  localStorage.setItem('swimming', swimming.value);
+  var inputFetch = document.getElementByTagName('fetch').value;
+  localStorage.setItem('fetch', fetch.value);
+  var inputWalks = document.getElementByTagName('walks').value;
+  localStorage.setItem('walks', walks.input);
+  var inputRunning = document.getElementByTagName('running').value;
+  localStorage.setItem('running', running.value);
+  var inputSunBathing = document.getElementByTagName('sunBathing').value;
+  localStorage.setItem('sunBathing', sunBathing.value);
+  var inputNeighborhood = document.getElementByTagName('neighborhood').value;
+  localStorage.setItem('neighborhood', neighborhood.value);
+  var inputagePref = document.getElementByTagName('agePref').value;
+  localStorage.setItem('agePref', agePref.value);
+  var inputSpayNeuPref = document.getElementByTagName('spayNeuPref').value;
+  localStorage.setItem('spayNeuPref', spayNeuPref.value);
+  var inputSize = document.getElementByTagName('sizes').value;
+  localStorage.setItem('sizes', sizes.value);
+}
+var inputForm = document.querySelectorAll('form');
+form.addEventListener('submit', submit);
