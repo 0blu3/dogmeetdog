f024adaa1ac54c7fe2b5ca162b556e352b82d3d6
0blu3 <braelynn.luedtke@gmail.com>
2016-11-08 11:49:37 -0800

    added html page for matching

---
+++ slider.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var totalMatches = 0;
+var highestMatches = [];
+
+
+
+function ShowHighPercentageMatches(){
+  if (Dog.matchPercentage > 70) {
+
+  }
+}
+
+for (var i = 0; i < 3; i++){
+  var currentMatches = highestMatches[i];
+
+
+
+  var htmlImageId = 'highestMatch' + i;
+}
