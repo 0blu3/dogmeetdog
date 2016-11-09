cc62a2b72994514e57c09d1cf8a0810be7d87220
mikemiksch <mikemiksch@gmail.com>
2016-11-09 12:09:54 -0800

    Radio buttons now working as intended.

--- js/form.js
+++ js/form.js
@@ -12,6 +12,7 @@ var user = {
   neighborhood: '',
   prefAge: '',
   prefSex: '',
+  prefSize: '',
   prefFixed: '',
   ratingSwimming: '',
   ratingFetch: '',
@@ -20,89 +21,44 @@ var user = {
   ratingSunbathing: '',
 };
 
-function getRatingValues(elementClass) {
+function getRadioNumericalValues(elementClass) {
   var radio = document.getElementsByClassName(elementClass);
   for (var i = 0; i < radio.length; i++) {
     if (radio[i].checked) {
       console.log(radio[i].value);
-      user.elementClass = parseInt(radio[i].value);
+      user[elementClass] = parseInt(radio[i].value);
+    }
+  }
+}
+
+function getRadioTextValues(elementClass) {
+  var radio = document.getElementsByClassName(elementClass);
+  for (var i = 0; i < radio.length; i++) {
+    if (radio[i].checked) {
+      console.log(radio[i].value);
+      user[elementClass] = radio[i].value;
     }
   }
 }
 
 function submit(event) {
   event.preventDefault();
-  console.log(event);
   user.ownerName = event.target.name.value;
-  console.log(user.ownerName);
   user.contactNumber = event.target.contact_number.value;
-  console.log(user.contactNumber);
   user.pupName = event.target.pupName.value;
-  console.log(user.pupName);
   user.breed = event.target.breed.value;
-  console.log(user.breed);
-
-  getRatingValues('ratingSwimming');
-
-  // var inputFemale = document.getElementById('female');
-  // if(inputFemale.checked) {
-  //   inputFemale.value;
-  //   console.log(inputFemale);
-  // }
-  //
-  // var inputAge = event.target.age.value;
-  // localStorage.setItem('age', age.value);
-  // console.log(inputAge);
-  //
-  // var inputSwimming = document.getElementById('swimming');
-  // if(inputSwimming.checked) {
-  //   inputSwimming.value;
-  //   console.log(inputSwimming.value);
-  // }
-  //
-  // var inputFetch = document.getElementById('fetch');
-  // if(inputFetch.checked) {
-  //   inputSwimming.value;
-  //   console.log(inputFetch.value);
-  // }
-  //
-  // var inputWalks = document.getElementById('walks');
-  // if(inputWalks.checked) {
-  //   inputWalks.value;
-  //   console.log(inputWalks.value);
-  // }
-  //
-  // var inputRunning = document.getElementById('running');
-  // if(inputRunning.checked) {
-  //   inputSwimming.value;
-  //   console.log(inputRunning.value);
-  // }
-  //
-  // var inputSunBathing = document.getElementById('sunBathing');
-  // if(inputSunBathing.checked) {
-  //   inputSunBathing.value;
-  //   console.log(inputSunBathing.value);
-  // }
-  //
-  // var inputAgePref = document.getElementById('agePref');
-  // if(inputAgePref.checked) {
-  //   inputAgePref.value;
-  //   console.log(inputAgePref.value);
-  // }
-  //
-  // var inputSpayNeuPref = document.getElementById('spayNeuPref');
-  // if(inputSpayNeuPref.checked) {
-  //   inputSpayNeuPref.value;
-  //   console.log(inputSpayNeuPref.value);
-  // }
-  //
-  // var inputPrefSize = document.getElementById('prefSize');
-  // if(inputPrefSize.checked) {
-  //   inputPrefSize.value;
-  //   console.log(inputPrefSize.value);
-  // }
-
-
+  user.age = parseInt(event.target.age.value);
+  getRadioTextValues('sex');
+  getRadioNumericalValues('ratingSwimming');
+  getRadioNumericalValues('ratingFetch');
+  getRadioNumericalValues('ratingWalks');
+  getRadioNumericalValues('ratingRunning');
+  getRadioNumericalValues('ratingSunbathing');
+  getRadioTextValues('prefAge');
+  getRadioTextValues('prefSex');
+  getRadioTextValues('prefSize');
+  getRadioTextValues('prefFixed');
+  localStorage.setItem('user', JSON.stringify(user));
 }
 var inputForm = document.getElementById('registration-form');
 inputForm.addEventListener('submit', submit);
